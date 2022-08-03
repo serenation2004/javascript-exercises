5e0c388cdc59c137b9bec0dd0be191b49d2756eb
Serene Liu <serene6197@gmail.com>
2022-08-03 13:08:21 +0100

    Complete Ex 3 All Pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = [];
+    for (let i = 0; i < string.length; i++){
+        array.push(string.charAt(i));
+    }
+    string = array.reverse().join('').toString();
+    return string;
 };
 
 // Do not edit below this line
